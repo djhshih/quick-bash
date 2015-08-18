71ec1f0fbb5706dfc22934fa854f590594c9e74b
David J. H. Shih <djh.shih@gmail.com>
2015-08-18 18:03:27 -0400

    Changed python code to conform with bash code.

--- code/word-count.py
+++ code/word-count.py
@@ -7,18 +7,23 @@ import fileinput
 word_counts = {}
 
 for line in fileinput.input():
+    # use list comprehension to generate character list
+    # join the characters into a string and split on space
     words = ''.join(
-        c for c in line.lower().replace('-', ' ')
-            if c.isalnum() or c.isspace()
+        # conver to lowercase; replace any non-alphnumeric character to space
+        c if c.isalnum() else ' ' for c in line.lower()
     ).split()
+    # count unique words
     for word in words:
         if word in word_counts:
             word_counts[word] += 1
         else:
             word_counts[word] = 1
 
+# sort words by descending frequency and ascending lexicographical order
 word_sorted = sorted(word_counts, key = lambda x: (-word_counts[x], x))
 
+# print word frequency table
 for word in word_sorted:
     print('   {} {}'.format(word_counts[word], word))
 
